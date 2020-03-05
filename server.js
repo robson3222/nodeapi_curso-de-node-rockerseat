cc4445929a2b5e593f6a7dc67e71c5560cc6186d
robson322 <robson3222@gmail.com>
2020-03-05 16:31:07 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+ const express = require('express');
+ const mongoose = require('mongoose');
+ 
+ //iniciando o app
+ const app = express();
+
+ // iniciando o DB
+ mongoose.connect('mongodb://localhost:27017/nodeapi', {   useNewUrlParser: true });
+
+
+
+
+// primeira rota
+app.get('/', (req, res) => {
+   res.send("certo");
+});
+
+
+
+
+
+ app.listen(3000);
